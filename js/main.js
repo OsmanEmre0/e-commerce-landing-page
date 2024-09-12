229d5edc963a2e9a14484d4515eddbf21aadb473
Osman Emre Sarıahmetoğlu <osariahnetoglu@gmail.com>
2024-09-13 02:47:25 +0300

    Card Structure

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-// swipper slider
+// swipper slider button next prev
 var swiper = new Swiper(".mySwiper", {
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+
+
+document.querySelector('.fa-arrow-right').addEventListener('click', function() {
+    document.querySelector('.view-more a').click();
 });
 
