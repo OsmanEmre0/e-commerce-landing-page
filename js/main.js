15bb0168eca890a28dccc9eff492f1299cf8c219
Osman Emre Sarıahmetoğlu <osariahnetoglu@gmail.com>
2024-09-18 21:37:09 +0300

    slide card structure was made

--- js/main.js
+++ js/main.js
@@ -6,9 +6,49 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-
-
-document.querySelector('.fa-arrow-right').addEventListener('click', function() {
+document.querySelector('.fa-arrow-right').addEventListener('click', function () {
     document.querySelector('.view-more a').click();
 });
+
+
+// Slider Card
+
+var swiper = new Swiper('.card-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: false,
+    grabCursor: true,
+    speed: 400,
+    navigation: {
+        nextEl: '.custom-swiper-next',
+        prevEl: '.custom-swiper-prev',
+    },
+    breakpoints: {
+        670: {
+            slidesPerView: 1,
+            spaceBetween: 8,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        },
+    },
+});
+
+
+
+
+
+
+
+
+
 
